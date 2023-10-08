0f2a4fb5027d6406f1e9e700d913b963433e4dd0
Mrxeex <rodrigobaalves.2004@gmail.com>
2023-10-08 17:20:17 -0300

    get funcionando sem model

---
+++ src/home/projeto-integrador-conexos-3/sciprt-id.js
@@ -0,0 +1,26 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     var openModalButton = document.getElementById('open-modal-button');
+//     var statusModal = new bootstrap.Modal(document.getElementById('statusModal'));
+//     var statusContent = document.getElementById('status-content');
+//     var codigoSolInput = document.getElementById('codigoSol');
+
+//     openModalButton.addEventListener('click', () => {
+//         var codigoSol = codigoSolInput.value;
+
+//         if (!codigoSol) {
+//             alert('Por favor, insira o código do pedido.');
+//             return;
+//         }
+
+//         axios.get(`http://localhost:3000/order/${codigoSol}`)
+//             .then(response => {
+//                 var status = response.data.status;
+
+//                 statusContent.textContent = `Status do pedido ${codigoSol}: ${status}`;
+//                 statusModal.show();
+//             })
+//             .catch(error => {
+//                 console.error('Erro na requisição GET:', error);
+//             });
+//     });
+// });
